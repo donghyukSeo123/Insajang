8c3debcf803379f8debc9dea46724c5cbcfcaa0a
서동혁 <ehdgur534@gmail.com>
2025-12-18 16:26:31 +0900

    Changes

---
+++ insajang-backend/src/main/java/com/project/insajang/InsajangApplication.java
@@ -0,0 +1,13 @@
+package com.project.insajang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsajangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsajangApplication.class, args);
+    }
+
+}
